c0b48924ae8c7c55fd7857cf557a251e326b8612
Tina Alex <78782318+tina-alex-leslie@users.noreply.github.com>
2022-04-24 19:07:16 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,417 @@
+import glob, os
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import sklearn.metrics as metrics
+
+def glob_concat(path, file_str):
+    '''
+    Looks for files in folder path and combines them into a DataFrame.
+    Also sets the index to be the SEQN column.
+    
+    Args:
+        path (str): Location on computer where files are located.
+        file_str (str): Specific search query of which files to find.
+        
+    Returns:
+        Displays the files that were found for verification and returns the 
+        DataFrame of all files after pd.concat is performed and the index is set.
+    
+    Example:
+        combined_df = glob_concat(r'File/File', '*.XPT')
+    '''
+    
+    # Find the files in the folders
+    files = glob.glob(os.path.join(path, file_str))
+
+    # Print the files for verification when running the function
+    display(files)
+
+    # Combining all the files into a DataFrame
+    df_files = [pd.read_sas(file) for file in files]
+    combined_df = pd.concat(df_files)
+
+    # Setting the index of the new DataFrame
+    combined_df.SEQN = combined_df.SEQN.astype('int64')
+    combined_df.set_index('SEQN', verify_integrity=True, inplace=True)
+    return combined_df
+
+
+def val_counts(df):
+    '''
+    Display value counts for all the columns in a DataFrame.
+
+    Args:
+        df (DataFrame): DataFrame to perform .value_counts() on each column. 
+
+    Returns:
+        Value counts for each column with some visual cues between columns.
+    '''
+
+    for col in df.columns:
+        print(f'{col} value counts', '\n')
+        display(df[col].value_counts(dropna=False))
+        print('--------------------------------------')
+
+
+def cols_tokeep(df, col_list):
+    '''
+    Makes a copy of the passed in DataFrame and drops all columns except those
+    specified in a list.
+    
+    Args:
+        df(DataFrame): DataFrame to take specific columns from.
+        col_list(list): List of columns in the DataFrame to be kept.
+    
+    Returns:
+        New DataFrame that has only the columns specified from the col_list.
+    
+    Example:
+        new_df = cols_tokeep(old_df, columns_list)
+    '''
+    df_copy = df.copy()
+    for col in df_copy.columns:
+        if col not in col_list:
+            df_copy.drop(columns=[col], inplace=True)
+        else:
+            pass
+    return df_copy
+
+
+def first_cancer_count(x):
+    '''
+    Function for creating a new column that is a count from the first cancer
+    information of people that have had a first cancer.
+    
+    Args:
+        x(row in DataFrame): Pass over each entry in DataFrame to count if a
+            first cancer is recorded.
+        
+    Returns:
+        1 if information of a first cancer exists and 0 if it does not.
+    
+    Example:
+        df.apply(first_cancer_count, axis=1)
+    '''
+
+    if x['first_cancer_type'] != 'None':
+        return 1
+    else:
+        return 0
+
+
+def second_cancer_count(x):
+    '''
+    Function for creating a new column that is a count from the second cancer
+    information of people that have had a second cancer.
+    
+    Args:
+        x(row in DataFrame): Pass over each entry in DataFrame to count if a
+            second cancer is recorded.
+        
+    Returns:
+        1 if information of a second cancer exists and 0 if it does not.
+    
+    Example:
+        df.apply(second_cancer_count, axis=1)
+    '''
+
+    if x['second_cancer_type'] != 'None':
+        return 1
+    else:
+        return 0
+
+
+def third_cancer_count(x):
+    '''
+    Function for creating a new column that is a count from the third cancer
+    information of people that have had a third cancer.
+    
+    Args:
+        x(row in DataFrame): Pass over each entry in DataFrame to count if a
+            third cancer is recorded.
+        
+    Returns:
+        1 if information of a third cancer exists and 0 if it does not.
+    
+    Example:
+        df.apply(third_cancer_count, axis=1)
+    '''
+
+    if x['third_cancer_type'] != 'None':
+        return 1
+    else:
+        return 0
+
+
+def plotting_counts(df, col, target='depression'):
+    '''
+    Generates countplot on a column in a dataframe.
+    
+    Args:
+        df (dataframe): Dataframe that contains the column and target to be 
+        plotted
+        col (str): Column name of the data to be plotted against the target
+        target (str): Target column of the dataframe
+        
+    Returns:
+        Count plot figure with bars grouped by the target
+    
+    Example:
+        plotting_counts(data, 'feature_name')
+    '''
+
+    # Sort the column values for plotting
+    order_list = list(df[col].unique())
+    order_list.sort()
+    
+    # Plot the figure
+    fig, ax = plt.subplots(figsize=(16,8))
+    x, y = col, target
+    ax = sns.countplot(x=x, hue=y, data=df, order=order_list)
+
+    # Set labels and title
+    plt.title(f'{col.title()} By Count {target.title()}', 
+              fontdict={'fontsize': 30})
+    plt.xlabel(f'{col.title()}', fontdict={'fontsize': 20})
+    plt.ylabel(f'{target.title()} Count', fontdict={'fontsize': 20})
+    plt.xticks(rotation=75)
+    return fig, ax
+
+
+def plotting_percentages(df, col, target='depression'):
+    '''
+    Generates catplot on a column in a dataframe that shows percentages at the
+    top of each bar.
+    
+    Args:
+        df (dataframe): Dataframe that contains the column and target to be 
+        plotted
+        col (str): Column name of the data to be plotted against the target
+        target (str): Target column of the dataframe
+        
+    Returns:
+        Catplot figure with bars grouped by the target and representing
+        percentages of the entries for each value
+    
+    Example:
+        plotting_percentages(data, 'feature_name')
+    '''
+    
+    x, y = col, target
+    
+    # Temporary dataframe with percentage values
+    temp_df = df.groupby(x)[y].value_counts(normalize=True)
+    temp_df = temp_df.mul(100).rename('percent').reset_index()
+
+    # Sort the column values for plotting    
+    order_list = list(df[col].unique())
+    order_list.sort()
+
+    # Plot the figure
+    sns.set(font_scale=1.5)
+    g = sns.catplot(x=x,y='percent',hue=y,kind='bar',data=temp_df, 
+                    height=8, aspect=2, order=order_list, legend_out=False)
+    g.ax.set_ylim(0,100)
+
+    # Loop through each bar in the graph and add the percentage value    
+    for p in g.ax.patches:
+        txt = str(p.get_height().round(1)) + '%'
+        txt_x = p.get_x() 
+        txt_y = p.get_height()
+        g.ax.text(txt_x,txt_y,txt)
+        
+    # Set labels and title
+    plt.title(f'{col.title()} By Percent {target.title()}', 
+              fontdict={'fontsize': 30})
+    plt.xlabel(f'{col.title()}', fontdict={'fontsize': 20})
+    plt.ylabel(f'{target.title()} Percentage', fontdict={'fontsize': 20})
+    plt.xticks(rotation=75)
+    return g
+
+
+def plot_num_cols(df, col, target='depression'):
+    '''
+    Generates 'boxen' type catplot on a column in a dataframe grouped by target
+    
+    Args:
+        df (dataframe): Dataframe that contains the column and target to be 
+        plotted
+        col (str): Column name of the data to be plotted against the target
+        target (str): Target column of the dataframe
+        
+    Returns:
+        Catplot 'boxen' figure split by the target 
+    
+    Example:
+        plotting_num_cols(data, 'feature_name')
+    '''
+    # Generating the figure
+    g = sns.catplot(x=target, y=col, data=df, kind='boxen', 
+                    height=7, aspect=2)
+
+    # Setting the title
+    plt.suptitle(f'{col.title()} and {target.title()}', fontsize=30, y=1.05)
+
+
+def make_classification_report(model, y_true, x_test, title=''):
+    
+    '''
+    Generate and return the classification report for a model.
+    
+    Args: 
+        Model (classification model): SKlearn compatable model.
+        y_true (series or array): True labels to compare predictions
+        x_test (dataframe or array): X data to generate predictions for
+        title (str): Title for the report
+        
+    Returns:
+        Dictionary of the classification results
+    
+    Example:
+        make_classification_report(logreg_model, y_test, X_test, 
+                                    title='Logistic Regression Model')
+        
+        '''
+    # Generate predictions
+    y_preds = model.predict(x_test)
+    print('__________________________________________________________________')
+    print(f'CLASSIFICATION REPORT FOR: \n\t{title}')
+    print('__________________________________________________________________')
+    print('\n')
+    
+    # Generate report
+    report = metrics.classification_report(y_true, y_preds, 
+                                target_names=['not depressed', 'depressed'])
+    report_dict = metrics.classification_report(y_true, y_preds, 
+                                                output_dict=True,
+                             target_names=['not depressed', 'depressed'])
+    
+    # Add the title to the report dictionary
+    report_dict['title'] = title
+    print(report)
+    print('__________________________________________________________________')
+    
+    return report_dict
+
+
+def plot_confusion_matrix(model, X, y, title=''):
+    '''
+    Plots the normalized confusion matrix for a model
+    
+    Args:
+        Model (classification model): SKlearn compatable model
+        X (dataframe or array): feature columns of a dataframe
+        y (series or array): target column of a dataframe
+        title (str): Title for the matrix
+    
+    Returns:
+        Plotted figure of the confusion matrix for the model
+    
+    Example:
+        plot_confusion_matrix(logreg_model, X_test, y_test, 
+        title='Logistic Regression Model')
+    '''
+    
+    # Plot the matrix with labels    
+    fig = metrics.plot_confusion_matrix(model, X, y, normalize='true', 
+                                        cmap='Greens',
+                                 display_labels=['not depressed', 'depressed']) 
+
+    # Remove grid lines
+    plt.grid(False)
+    
+    # Set title
+    plt.title(f'Confusion Matrix For {title}', fontdict={'fontsize':17})
+    plt.show()
+    print('__________________________________________________________________')
+    return fig
+
+
+def plot_roc_curve(model, xtest, ytest, title=''):
+    '''
+    Plots the precision-recall curve for a model
+    
+    Args:
+        Model (classification model): SKlearn compatable model
+        xtest (dataframe or array): feature columns of the test set
+        ytest (series or array): target column of the test set
+        
+    Returns:
+        Plotted figure of ROC curve for the model
+    
+    Example:
+        plot_roc_curve(classification_model, X_test, y_test)
+    '''
+    # Creating the plot
+    fig, ax = plt.subplots(figsize=(8,6), ncols=1)
+    roc_plot = metrics.plot_roc_curve(model, xtest, ytest, ax=ax)
+
+    # Setting the title of the plot
+    ax.set_title(f'ROC Curve For {title}', 
+                 fontdict={'fontsize':17})
+
+    # Setting a legend for the plot
+    ax.legend()
+    plt.show();
+    
+    return fig
+
+
+def plot_top_features(model, xtrain, title=''):
+    '''
+    Plots the top important features of a tree based model
+    
+    Args:
+        Model (classification model): SKlearn compatable model
+        xtrain (dataframe or array): feature columns for the training set
+        title (str): Title for the plot
+        
+    Returns:
+        Plotted figure of feature importances for the model
+    
+    Example:
+        plot_top_features(rf_model, X_train, title='Random Forest Model')
+    '''
+
+    # Turn the feature importances into a series 
+    importances = pd.Series(model.feature_importances_, index=xtrain.columns)
+    
+    # Plot the top most important features
+    importances.nlargest(20).sort_values().plot(kind='barh')
+    plt.title(f'Most Important Features For {title}', fontdict={'fontsize':17})
+    plt.xlabel('Importance')
+    return importances.sort_values(ascending=False)
+
+
+def evaluate_model(model, xtrain, xtest, ytest, tree=False, title=''):
+    '''
+    Runs all the evaluation functions on a model including the classification 
+    report, confusion matrix, ROC plot, and a top features plot if the 
+    model is tree based.
+    
+    Args:
+        model (classification model): SKlearn compatable model
+        xtrain (dataframe or array): feature columns of the training set
+        xtest (dataframe or array): feature columns of the test set
+        ytest (series or array): target column of the test set
+        tree (boolean): if the model is tree based or not
+        title (str): Title for the model
+    
+    Returns:
+        The classification report, confusion matrix, precision recall plot, and 
+        top features plot if tree=True
+    
+    Example:
+        evaluate_model(logreg_model, X_train, X_test, y_test,
+                        title='Logistic Regression Model')
+        
+    '''
+    
+    make_classification_report(model, ytest, xtest, title=title)
+    plot_confusion_matrix(model, xtest, ytest, title=title)
+    plot_roc_curve(model, xtest, ytest, title=title)
+    
+    # Feature importance can only be run on tree based models
+    if tree:
+        plot_top_features(model, xtrain, title=title)
